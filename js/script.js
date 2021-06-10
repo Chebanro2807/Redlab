8e99a8128413d20d793a2fa063ddfc8ef0ef4e97
Chebanro2807 <Chebanro2807@ukr.net>
2021-06-10 20:38:50 +0300

    working with button

--- js/script.js
+++ js/script.js
@@ -58,3 +58,38 @@ class RedLab {
 }
 
 new RedLab();
+
+
+let ball = document.querySelector('.oreder-cleaning');
+let mouseX = 0;
+let mouseY = 0; 
+
+let ballX = 300;
+let ballY = 300; 
+
+let speed = 0.1;
+
+
+function animate() {
+    let distX = mouseX - ballX;
+    let distY = mouseY - ballY;
+
+    ballX = ballX + (distX * speed);
+    ballY = ballY + (distY * speed);
+    
+    if (ballX > 600 || ballX < 0 || ballY > 600 || ballY < 0) {
+        ball.style.cssText = "left: 50%; top: 50%;"
+    } else {
+    ball.style.cssText = "left:" + ballX + "px; top:" + ballY + "px;"
+    }
+    requestAnimationFrame(animate);
+}
+
+
+
+
+document.querySelector('.oreder-cleaning-wrap').addEventListener("mousemove", function(event){
+    mouseX = event.pageX+300-(window.innerWidth/2);
+    mouseY = event.pageY-435;
+    animate();
+})
