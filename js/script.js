a78bb80b27df4037034d888c65c31c6590a41957
Chebanro2807 <Chebanro2807@ukr.net>
2021-06-11 15:13:08 +0300

    done with button order

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 class RedLab {
     constructor () {
         this.header = document.querySelector(".header");
+        this.orderWrap = document.querySelector(".oreder-cleaning");
         this.orderText = document.querySelector(".oreder-cleaning__text");
         this.ball = document.querySelector(".oreder-cleaning__ball");
         this.ballwrap = document.querySelector(".oreder-cleaning__wrap");
+        this.yEvent = 0;
 
         this.ballwrap.onmousemove = this.moveBall.bind(this);
         this.createAppearScrollAnimation();
@@ -12,12 +14,18 @@ class RedLab {
     }
 
     moveBall(event){
-        let YEvent = event.clientY+202.5-660;
-        let xEvent = event.clientX+202.5-(window.innerWidth/2)
+        if (window.scrollY === 0) {
+            this.yEvent = event.clientY+202.5-660;
+        } else {
+            this.yEvent = event.clientY+202.5-(window.innerHeight/10*8.7);
+        }
+        let xEvent = event.clientX+217.5-(window.innerWidth/2);
         let x = xEvent * 100 / this.ballwrap.clientWidth + "%";
-        let y = YEvent * 100 / this.ballwrap.clientHeight + "%";
+        let y = this.yEvent * 100 / this.ballwrap.clientHeight + "%";
+        console.log(xEvent)
+        console.log(x)
         if (Number(x.slice(0, -1)) < 0 || Number(y.slice(0, -1)) < 0) {
-            this.ball.style.cssText = "left: 50%; top: 50%; transform: translate(-50%, -50%);" 
+            this.ball.style.cssText = "" 
         }else{
             this.ball.style.cssText = "left:" + x + "; top:" + y + "; transform: translate(-" + x + ", -" + y + ");" 
             this.orderText.style.cssText = "left:" + x + "; top:" + y + "; transform: translate(-" + x + ", -" + y + ");" 
@@ -25,7 +33,12 @@ class RedLab {
     }
 
     returnInPozition () {
-        this.ball.style.cssText = "left: 50%; top: 50%; transform: translate(-50%, -50%);";
+        if (window.scrollY === 0) {
+            this.ball.style.cssText = "left: 50%; top: 50%; transform: translate(-50%, -50%);";
+        } else {
+            this.ball.style.cssText = "top: 86%;";
+        }
+        
         this.orderText.style.cssText = "";
     }
 
@@ -34,10 +47,14 @@ class RedLab {
             this.header.classList.remove("white");
             this.ball.classList.remove("small-circle");
             this.orderText.classList.remove("small-text");
+            this.orderWrap.classList.remove("small-wrap");
+            this.ball.style.cssText = "top: 50%;";
         } else {
             this.header.classList.add("white");
             this.ball.classList.add("small-circle");
             this.orderText.classList.add("small-text");
+            this.orderWrap.classList.add("small-wrap");
+            this.ball.style.cssText = "top: 86%;";
         }
     }
 
